d80fadeb9611997a08939e83a23641e9002e47b8
Bard <59461181+prove-ability@users.noreply.github.com>
2022-10-24 02:11:04 +0900

    feat: amplify init
    resolves: #18
    ref: #17

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  api: {
+    landofiron: {
+      GraphQLAPIKeyOutput: 'string'
+      GraphQLAPIIdOutput: 'string'
+      GraphQLAPIEndpointOutput: 'string'
+    }
+  }
+}
---
+++ src/models/schema.js
@@ -0,0 +1,61 @@
+export const schema = {
+  models: {
+    Post: {
+      name: 'Post',
+      fields: {
+        id: {
+          name: 'id',
+          isArray: false,
+          type: 'ID',
+          isRequired: true,
+          attributes: [],
+        },
+        Description: {
+          name: 'Description',
+          isArray: false,
+          type: 'String',
+          isRequired: false,
+          attributes: [],
+        },
+        createdAt: {
+          name: 'createdAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+        updatedAt: {
+          name: 'updatedAt',
+          isArray: false,
+          type: 'AWSDateTime',
+          isRequired: false,
+          attributes: [],
+          isReadOnly: true,
+        },
+      },
+      syncable: true,
+      pluralName: 'Posts',
+      attributes: [
+        {
+          type: 'model',
+          properties: {},
+        },
+        {
+          type: 'auth',
+          properties: {
+            rules: [
+              {
+                allow: 'public',
+                operations: ['create', 'update', 'delete', 'read'],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+  enums: {},
+  nonModels: {},
+  version: 'fa4e9dfffa4be93ee342250b3547259d',
+}
