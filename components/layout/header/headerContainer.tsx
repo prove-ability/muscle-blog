a1e8cb8add472626184a4f65e4772df7877fa4a2
Bard <59461181+prove-ability@users.noreply.github.com>
2022-10-26 01:48:15 +0900

    feat: 스크롤 이벤트 추가 - 헤더, 푸터 숨김 처리 작업
    레이아웃 컴포넌트에 스크롤 이벤트 추가
    그외 각종 css 작업
    
    resolves: #23
    ref: #19

--- components/layout/header/headerContainer.tsx
+++ components/layout/header/headerContainer.tsx
@@ -5,5 +5,9 @@ interface Props {
 }
 
 export default function HeaderContainer({ children }: Props) {
-  return <header className="p-4 border-2 border-rose-500">{children}</header>
+  return (
+    <header className="fixed top-0 z-10 w-full p-4 transition-all duration-500 bg-opacity-0 border-2 border-rose-500">
+      {children}
+    </header>
+  )
 }
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
 
 import Footer from './footer'
 import Header from './header'
@@ -9,8 +9,35 @@ type Props = {
 }
 
 const Layout = ({ children }: Props) => {
+  useEffect(() => {
+    let lastScrollTop = 0
+    const header = document.querySelector('header')
+    const main = document.getElementById('main')
+    const indexDom = document.querySelector('.snap-mandatory')
+
+    // 헤더 숨기는 함수
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || indexDom?.scrollTop || 0
+      if (scrollTop > lastScrollTop)
+        header?.setAttribute('style', 'top: -60px;')
+      else header?.setAttribute('style', 'top: 0px;')
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop // For Mobile or negative scrolling
+    }
+
+    // 스크롤 위로 올릴때
+    const handleScrollEnd = (e: unknown) => {
+      if ((e as WheelEvent).deltaY < 0) window?.scrollTo(0, 0)
+    }
+
+    indexDom?.addEventListener('scroll', handleScroll)
+    main?.addEventListener('mousewheel', handleScrollEnd)
+    return () => {
+      indexDom?.removeEventListener('scroll', handleScroll)
+      main?.removeEventListener('mousewheel', handleScrollEnd)
+    }
+  }, [])
   return (
-    <div className="flex flex-col min-h-screen">
+    <div id="main" className="flex flex-col min-h-screen scrollbar-hide">
       <Header>
         <Header.Wrapper>
           <Header.Title>제목</Header.Title>
